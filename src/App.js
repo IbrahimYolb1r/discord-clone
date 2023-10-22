abfa0f2cc4eded0daba21634f654f0c8737b0aaf
IbrahimYolb1r <85681843+IbrahimYolb1r@users.noreply.github.com>
2023-10-23 05:46:09 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
           uid: authUser.uid,
           photo: authUser.photoURL,
           email: authUser.email,
-          displayName: authUser.displayName
+          displayName: authUser.displayName,
         }))
       } else {
         dispatch(logout())
